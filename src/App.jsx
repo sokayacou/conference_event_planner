dd1f8a72e05a9006b1a606af2d355edc68005352
Yacouba Sokanda <161978781+sokayacou@users.noreply.github.com>
2026-07-14 12:06:44 +0000

    Change initial state of showVenue to true

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ConferenceEvent from "./ConferenceEvent";
 import AboutUs from "./AboutUs";
 
 function App() {
-  const [showVenue, setShowVenue] = useState(false);
+  const [showVenue, setShowVenue] = useState(true);
 
   const handleGetStarted = () => {
     setShowVenue(true);
